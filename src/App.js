5a2f5829f714dc7cf8746b454e7097b567c0b8b9
ypradeeprao <39065049+ypradeeprao@users.noreply.github.com>
2023-01-16 13:04:36 -0600

    Revert "fdf"

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState,useEffect, createRef } from 'react';
-import { createChannel, createClient, RtmMessage } from 'agora-rtm-react';
-const useClient = createClient('58389bfcba704a8aa0f964e67a4ca6be');
-const useChannel = createChannel('channelName');
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect, createRef } from "react";
 
 function App() {
-  const client = useClient();
-  const testChannel = useChannel(client);
-
   const [compstate, setCompstate] = useState({
     showui: true,
-    mainpeerconnecionObjArray:[]
+    mainpeerconnecionObjArray: [],
   });
 
   useEffect(() => {
-   // getData();
-
+    // getData();
   }, []);
 
   let getData = async (methodprops) => {
     let totaldata = await localStorage.getItem("totaljson");
-    if(totaldata){
-    let totaldataJson = JSON.parse(totaldata);
-    let meetingname = totaldataJson["meetingname"];
-    let personname = totaldataJson["personname"];
-    showui({meetingname:meetingname,personname:personname });
+    if (totaldata) {
+      let totaldataJson = JSON.parse(totaldata);
+      let meetingname = totaldataJson["meetingname"];
+      let personname = totaldataJson["personname"];
+      showui({ meetingname: meetingname, personname: personname });
     }
-  }
+  };
 
   let showui = async (methodprops) => {
     let compstatejs = JSON.parse(JSON.stringify(compstate));
@@ -43,185 +36,158 @@ function App() {
 
   let handleChange = async (methodprops) => {
     let { type, order, value } = methodprops;
-  console.log(methodprops);
+    console.log(methodprops);
 
-    if(type == "meetingname" ){
-      await showui({"meetingname":value});
+    if (type == "meetingname") {
+      await showui({ meetingname: value });
     }
-    if(type == "personname"){
-      await showui({"personname":value});
+    if (type == "personname") {
+      await showui({ personname: value });
     }
-  }
+  };
 
   let handleClick = async (methodprops) => {
     let { type, order } = methodprops;
     console.log(methodprops);
-  
+
     let totaldata = await localStorage.getItem("totaljson");
     let totaldataJson = {};
-    if(totaldata){
+    if (totaldata) {
       totaldataJson = JSON.parse(totaldata);
     }
 
-   if(type == "createmeeting"){
-    if(totaldataJson && totaldataJson.meetings){
-      totaldataJson.meetings[compstate.meetingname] = {}
-     
-        
-    }
-    else{
-      totaldataJson.meetings = {};
-      totaldataJson.meetings[compstate.meetingname] = {}
-    }
-
-
-    await localStorage.setItem(
-      "totaljson",
-      JSON.stringify(totaldataJson)
-    );
-  }
-  if(type == "joinmeeting"){
-
-    await client.login({ uid: 'userId' });
-    await testChannel.join();
-   // const message = client.createMessage({ "text", messageType: 'TEXT' });
-   // await testChannel.sendMessage(message);
-
-    if(totaldataJson && totaldataJson.meetings 
-      && totaldataJson.meetings[compstate.meetingname]){
-    
-   let joinednames = [];
-   if(totaldataJson.meetings[compstate.meetingname].joinednames){
-    joinednames = totaldataJson.meetings[compstate.meetingname].joinednames;
-  }
-
-  let joinedname ={
-    name:compstate.personname,
-    mainpeerconnections:[]
-    };
-   
-    joinednames.push(joinedname);
-     totaldataJson.meetings[compstate.meetingname].joinednames = joinednames;
-
-
-     if(totaldataJson.meetings[compstate.meetingname].joinednames){
-      joinednames = totaldataJson.meetings[compstate.meetingname].joinednames;
-  
-      let mainpeerconnections = [];
-    
-      for(let i=0; i< joinednames.length; i++){
-        if(compstate.personname == joinednames[i].name && joinednames[i].mainpeerconnections){
-          mainpeerconnections = joinednames[i].mainpeerconnections;
-        }
+    if (type == "createmeeting") {
+      if (totaldataJson && totaldataJson.meetings) {
+        totaldataJson.meetings[compstate.meetingname] = {};
+      } else {
+        totaldataJson.meetings = {};
+        totaldataJson.meetings[compstate.meetingname] = {};
       }
-     
-      for(let i=0; i< joinednames.length; i++){
-        if(compstate.personname != joinednames[i].name){
-  
-          let mainpeerconnecionobj ={
-            meetingname:compstate.meetingname,
-            fromname:compstate.personname,
-             toname:joinednames[i].name, 
-             peerconnecionobject:{}
-            };
-  
-  
-        mainpeerconnections.push(mainpeerconnecionobj);
+
+      await localStorage.setItem("totaljson", JSON.stringify(totaldataJson));
+    }
+    if (type == "joinmeeting") {
+      if (
+        totaldataJson &&
+        totaldataJson.meetings &&
+        totaldataJson.meetings[compstate.meetingname]
+      ) {
+        let joinednames = [];
+        if (totaldataJson.meetings[compstate.meetingname].joinednames) {
+          joinednames =
+            totaldataJson.meetings[compstate.meetingname].joinednames;
         }
-        joinednames[i].mainpeerconnections = mainpeerconnections;
-        }
-        
-  
+        let mainpeerconnections = [];
+       
+
      
-      
-  
-  
+
+        if (totaldataJson.meetings[compstate.meetingname].joinednames) {
+          joinednames =
+            totaldataJson.meetings[compstate.meetingname].joinednames;
+
+          for (let i = 0; i < joinednames.length; i++) {
+            if (compstate.personname != joinednames[i].name) {
+             
+              
+              let mainpeerconnecionobj = {
+                meetingname: compstate.meetingname,
+                fromname: compstate.personname,
+                toname: joinednames[i].name,
+                viaservername: "",
+                peerconnecionobject: {},
+              };
+
+              mainpeerconnections.push(mainpeerconnecionobj);
+             
+            }
+          }
+        }
+        let joinedname = {
+          name: compstate.personname,
+          mainpeerconnections: mainpeerconnections,
+        };
+        joinednames.push(joinedname);
+        totaldataJson.meetings[compstate.meetingname].joinednames = joinednames;
+
+        await localStorage.setItem("totaljson", JSON.stringify(totaldataJson));
+      } else {
+        alert("no meeting exists");
+      }
     }
+    if (type == "quitmeeting") {
+      if (
+        totaldataJson &&
+        totaldataJson.meetings &&
+        totaldataJson.meetings[compstate.meetingname]
+      ) {
+        let joinednames = [];
+        if (totaldataJson.meetings[compstate.meetingname].joinednames) {
+          joinednames =
+            totaldataJson.meetings[compstate.meetingname].joinednames;
+        }
 
+        let joinednamesU = [];
+        for (let i = 0; i < joinednames.length; i++) {
+          if (compstate.personname != joinednames[i].name) {
+            let mainpeerconnections = joinednames[i].mainpeerconnections;
+            let mainpeerconnectionsU = [];
+            for (let j = 0; j < mainpeerconnections.length; j++) {
+              if (
+                mainpeerconnections[j].fromname == compstate.personname ||
+                mainpeerconnections[j].toname == compstate.personname
+              ) {
+              } else {
+                mainpeerconnectionsU.push(mainpeerconnections[j]);
+              }
+            }
+            joinednames[i].mainpeerconnections = mainpeerconnectionsU;
+            joinednamesU.push(joinednames[i]);
+          }
+        }
 
+        totaldataJson.meetings[compstate.meetingname].joinednames =
+          joinednamesU;
 
-      
-      
-        await localStorage.setItem(
-          "totaljson",
-          JSON.stringify(totaldataJson)
-        );
-        
+        await localStorage.setItem("totaljson", JSON.stringify(totaldataJson));
+      } else {
+        alert("no meeting exists");
+      }
     }
-    else{
-      alert("no meeting exists");
-    }
-
-
-
-  }
-  if(type == "quitmeeting"){
-    if(totaldataJson && totaldataJson.meetings 
-      && totaldataJson.meetings[compstate.meetingname]){
-    
-   let joinednames = [];
-   if(totaldataJson.meetings[compstate.meetingname].joinednames){
-    joinednames = totaldataJson.meetings[compstate.meetingname].joinednames;
-  }
-
-    let joinednamesU = [];
-   for(let i=0; i< joinednames.length; i++){
-   if(compstate.personname != joinednames[i].name){
-    joinednamesU.push(joinednames[i]);
-   }
-   }
-   
-     totaldataJson.meetings[compstate.meetingname].joinednames = joinednamesU;
-
-
-
-      
-        await localStorage.setItem(
-          "totaljson",
-          JSON.stringify(totaldataJson)
-        );
-        
-    }
-    else{
-      alert("no meeting exists");
-    }
-
-
-
-  }
-  }
+  };
 
   console.log(compstate);
   if (compstate.showui != true) {
     return <></>;
   } else {
-  return (
-   <div>
-    meetingname
-    <input   onChange={(e) =>
+    return (
+      <div>
+        meetingname
+        <input
+          onChange={(e) =>
             handleChange({
               type: "meetingname",
               value: e.target.value,
             })
           }
-           defaultValue={compstate.meetingname}
-            />
-    personname
-    <input   onChange={(e) =>
+          defaultValue={compstate.meetingname}
+        />
+        personname
+        <input
+          onChange={(e) =>
             handleChange({
               type: "personname",
               value: e.target.value,
             })
           }
           defaultValue={compstate.personname}
-          />
-    <div onClick={() => handleClick({type:"createmeeting"})} >create</div>
-    <div  onClick={() => handleClick({type:"joinmeeting"})} >Join</div>
-    <div  onClick={() => handleClick({type:"quitmeeting"})} >quit</div>
-    <div  onClick={() => handleClick({type:"deletemeeting"})} >delete</div>
-   </div>
-  
-  );
+        />
+        <div onClick={() => handleClick({ type: "createmeeting" })}>create</div>
+        <div onClick={() => handleClick({ type: "joinmeeting" })}>Join</div>
+        <div onClick={() => handleClick({ type: "quitmeeting" })}>quit</div>
+        <div onClick={() => handleClick({ type: "deletemeeting" })}>delete</div>
+      </div>
+    );
   }
 }
 
